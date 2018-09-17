cea79f99824bee1916da0405d2c462a7f58af101
juzi <hanks0407@163.com>
2018-09-17 18:20:01 +0800

    正则，每行去"|,

--- src/main/java/read_file.java
+++ src/main/java/read_file.java
@@ -1,12 +1,12 @@
+import org.python.antlr.ast.Str;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.regex.*;
 
 public class read_file {
 
-
     public static void main(String[] args) {
-
         try {
             BufferedReader reader = new BufferedReader(new FileReader("D:\\模型\\风控数据\\test_data.csv"));//换成你的文件名
             String header = reader.readLine();//第一行信息，为标题信息，不用，如果需要，注释掉
@@ -22,25 +22,33 @@ public class read_file {
                 Pattern p = Pattern.compile(arg1);
                 Matcher m = p.matcher(last);
                 StringBuffer sb = new StringBuffer();
-
                 while (m.find()){
                     m.appendReplacement(sb,m.group().replace(",","_").replace("\"",""));
                 }
                 m.appendTail(sb);
                 last = sb.toString();
 
-                //int value = Integer.parseInt(last);//如果是数值，可以转化为数值
+                String arg2 = "\"[男|女].*\"";
+                Pattern p1 = Pattern.compile(arg2);
+                Matcher m1 = p1.matcher(last);
+                StringBuffer sb1 = new StringBuffer();
+                while (m1.find()){
+                    m1.appendReplacement(sb1,m1.group().replace(",","_").replace("\"",""));
+                }
+                m1.appendTail(sb1);
+                last = sb1.toString();
+
                 String [] last_ = last.split(",");
-                if(last_.length == 197){
-//                    System.out.println("用户编号："+last_[1]);
+                if(last_.length != 197){
+                    System.out.println("用户编号："+last_[1]);
                     for (String tt:last_
                          ) {
                         System.out.print(tt+" ");
                     }
                 }
-//                System.out.println("字符串长度"+last_.length);
+                System.out.print("\n");
+                System.out.println("字符串长度"+last_.length);
             }
-//            System.out.println("数量："+i);
         } catch (Exception e) {
             e.printStackTrace();
         }
